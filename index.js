e1f7fd0ef79260314caa0d2e0901223a35b96d6e
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-27 17:14:05 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+/*jshint browser: true */
+
+/**
+ * Gets the current value of a style property for a given DOM element.
+ * 
+ * @name exports
+ * @function
+ * @param el {Object} The DOM element.
+ * @param propName {String} The name of the property.
+ * @return {String} The current value of the element's style and given property. 
+ */
+module.exports = function(el, propName) {
+  return el.currentStyle
+    ? el.currentStyle[propName]
+    : window.getComputedStyle
+      ? document.defaultView.getComputedStyle(el, null).getpropNameertyValue(propName)
+      : null;
+};
